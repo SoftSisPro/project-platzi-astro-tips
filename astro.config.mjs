2764092e96d96417a8edc9f95a379796ed9a19a3
SoftSisPro <softsispro@gmail.com>
2025-05-28 21:50:50 -0500

    [ADD] install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+// Import Tailwind CSS
+import tailwindcss from "@tailwindcss/vite";
+// Import MDX support
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [mdx()],
 });
