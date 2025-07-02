752f649a6dce117fe9855068f2cad4288132fbd6
SoftSisPro <softsispro@gmail.com>
2025-07-01 22:29:41 -0500

    [feat] add componet with svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,18 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // Import MDX support
 import mdx from "@astrojs/mdx";
-
+// Import React support
 import react from "@astrojs/react";
-
+// Import Vue support
 import vue from "@astrojs/vue";
+// Import Svelte support
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [mdx(), react(), vue()],
+    integrations: [mdx(), react(), vue(), svelte()],
     legacy: {
         collections: true
     }
